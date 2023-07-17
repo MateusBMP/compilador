b4ffa8405b924324d9457e6126950b7bbaad1cc7
MateusBMP <bmp.mateus@gmail.com>
2023-07-17 17:59:41 -0300

    Adding Antlr4 to project

--- src/main/java/com/arcproject/compilador/App.java
+++ src/main/java/com/arcproject/compilador/App.java
@@ -1,5 +1,7 @@
 package com.arcproject.compilador;
 
+import com.arcproject.compilador.ArrayInit;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ArrayInit.main(args);
     }
 }
